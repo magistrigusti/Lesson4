0067652f48c791ea90bb4bbf7d39d77aa6a07900
Magistry <Magistrigusti@yandex.ru>
2024-07-21 10:31:03 +0300

    first commit

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -1,18 +1,25 @@
-import { toNano } from '@ton/core';
+import { Address, beginCell, Cell, toNano } from '@ton/core';
 import { NftCollection } from '../wrappers/NftCollection';
 import { NetworkProvider } from '@ton/blueprint';
 
+function createOffchainContent(link: string) {
+    return beginCell().storeUint(1, 8).storeStringTail(link).endCell()
+}
+
 export async function run(provider: NetworkProvider) {
-    const nftCollection = provider.open(await NftCollection.fromInit());
+    const nftCollection = provider.open(await NftCollection.fromInit(provider.sender().address!!, createOffchainContent("https://s.getgems.io/nft/b/c/64427bdf371066c7ee000d99/meta.json"), createOffchainContent("https://s.getgems.io/nft/b/c/64427bdf371066c7ee000d99/")));
 
     await nftCollection.send(
         provider.sender(),
         {
-            value: toNano('0.05'),
+            value: toNano('0.08'),
         },
         {
-            $$type: 'Deploy',
-            queryId: 0n,
+            $$type: "MintNft",
+            owner: Address.parse("0QCRFHD0ndG_wVIN00H_lFVCl5Ce_perCRZU27F2idoAzoKz"),
+            forwardAmount: toNano("0.04"),
+            content: Cell.EMPTY,
+            index: 1n
         }
     );
 
